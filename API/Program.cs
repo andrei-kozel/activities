f1caca69073e6e8f75cd3a1f00a10dcdc8c00c85
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-12-26 21:06:34 +0100

    added automapper

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,8 @@
 using Application.Activities;
+using Application.Core;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,25 +14,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
-  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-}); 
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddCors(opt =>
 {
-  opt.AddPolicy("CorsPolicy", policy =>
-  {
-    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:5173");
-  });
+    opt.AddPolicy("CorsPolicy",
+        policy => { policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:5173"); });
 });
 builder.Services.AddMediatR(typeof(List.Handler));
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
 app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
@@ -45,14 +46,14 @@ var services = scope.ServiceProvider;
 /* Migrating the database and seeding it with data. */
 try
 {
-  var context = services.GetRequiredService<DataContext>();
-  await context.Database.MigrateAsync();
-  await Seed.SeedData(context);
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
 }
 catch (Exception ex)
 {
-  var logger = services.GetRequiredService<ILogger<Program>>();
-  logger.LogError(ex, "An error occured during migration");
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
 }
 
 app.Run();
