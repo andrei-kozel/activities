ffb8fe5e6794e734e82e5c72db2913575aeac5c9
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-12-26 21:00:20 +0100

    edit activity handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -6,45 +6,52 @@ namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
-  /// <summary>
-  ///     This function returns a list of activities from the database.
-  /// </summary>
-  /// <returns>
-  ///     A list of activities
-  /// </returns>
-  [HttpGet]
+    /// <summary>
+    ///     This function returns a list of activities from the database.
+    /// </summary>
+    /// <returns>
+    ///     A list of activities
+    /// </returns>
+    [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
         return await Mediator.Send(new List.Query());
     }
 
-  /// <summary>
-  ///     It gets the activity with the given id.
-  /// </summary>
-  /// <param name="Guid">
-  ///     A globally unique identifier (GUID) is a 128-bit integer (16 bytes) that can
-  ///     be used across all computers and networks wherever a unique identifier is required. Such an
-  ///     identifier has a very low probability of being duplicated.
-  /// </param>
-  /// <returns>
-  ///     The activity with the given id.
-  /// </returns>
-  [HttpGet("{id}")]
+    /// <summary>
+    ///     It gets the activity with the given id.
+    /// </summary>
+    /// <param name="Guid">
+    ///     A globally unique identifier (GUID) is a 128-bit integer (16 bytes) that can
+    ///     be used across all computers and networks wherever a unique identifier is required. Such an
+    ///     identifier has a very low probability of being duplicated.
+    /// </param>
+    /// <returns>
+    ///     The activity with the given id.
+    /// </returns>
+    [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
         return await Mediator.Send(new Details.Query { Id = id });
     }
 
-  /// <summary>
-  ///     It takes an activity object, sends it to the mediator, and returns the result
-  /// </summary>
-  /// <param name="Activity">This is the model that we are passing to the API.</param>
-  /// <returns>
-  ///     The result of the command being sent to the mediator.
-  /// </returns>
-  [HttpPost]
+    /// <summary>
+    ///     It takes an activity object, sends it to the mediator, and returns the result
+    /// </summary>
+    /// <param name="Activity">This is the model that we are passing to the API.</param>
+    /// <returns>
+    ///     The result of the command being sent to the mediator.
+    /// </returns>
+    [HttpPost]
     public async Task<IActionResult> CreateActivity([FromBody] Activity activity)
     {
         return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+    {
+        activity.Id = id;
+        return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
     }
 }
