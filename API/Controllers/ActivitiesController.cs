2b10cd44760e02c588555fdab6dc9077f5925d42
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-12-26 20:32:25 +0100

    refactor with MediatR

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,20 +1,13 @@
-﻿using Domain;
+﻿using Application.Activities;
+using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
-  private readonly DataContext _context;
-
-  /* A constructor. */
-  public ActivitiesController(DataContext context)
-  {
-    _context = context;
-  }
-
+  
   /// <summary>
   /// This function returns a list of activities from the database.
   /// </summary>
@@ -24,7 +17,7 @@ public class ActivitiesController : BaseApiController
   [HttpGet]
   public async Task<ActionResult<List<Activity>>> GetActivities()
   {
-    return await _context.Activities.ToListAsync();
+    return await Mediator.Send(new List.Query());
   }
 
   /// <summary>
@@ -39,6 +32,6 @@ public class ActivitiesController : BaseApiController
   [HttpGet("{id}")]
   public async Task<ActionResult<Activity>> GetActivity(Guid id)
   {
-    return await _context.Activities.FindAsync(id);
+    return await Mediator.Send(new Details.Query{Id=id});
   }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using Application.Activities;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +14,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
+}); 
 builder.Services.AddCors(opt =>
 {
   opt.AddPolicy("CorsPolicy", policy =>
@@ -21,7 +22,7 @@ builder.Services.AddCors(opt =>
     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:5173");
   });
 });
-
+builder.Services.AddMediatR(typeof(List.Handler));
 
 var app = builder.Build();
 
