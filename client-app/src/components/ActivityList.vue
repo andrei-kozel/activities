4971f8897f5eebe09b19a2cdd832fe4a65342877
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-03 21:36:08 +0100

    activities list component

---
+++ client-app/src/components/ActivityList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mt-6">
+    <ActivityItem v-for="activity in activities" :activity="activity" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { IActivity } from "@/types/Activity.interface";
+import ActivityItem from "@/components/ActivityItem.vue";
+
+export default defineComponent({
+  name: "ActivityList",
+  props: {
+    activities: Array<IActivity>,
+  },
+  components: {
+    ActivityItem,
+  },
+});
+</script>
+
+<style scoped></style>
