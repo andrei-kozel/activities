73909767b635963e0d58d746d4d4c20c558fb139
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-03 21:36:39 +0100

    refactor

--- client-app/src/App.vue
+++ client-app/src/App.vue
@@ -2,31 +2,19 @@
   <header>
     <Navigation />
   </header>
-  <div v-for="activity in activities" :key="activity.id">
-    {{ activity.title }}
-  </div>
+
   <RouterView />
 </template>
 
 <script lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import Navigation from "./components/Navigation.vue";
 import { defineComponent } from "vue";
-import { getActivities } from "./services/activitiesApi";
-import type { IActivity } from "./types/Activity.interface";
 
 export default defineComponent({
   components: {
     Navigation,
     RouterView,
-  },
-  data() {
-    return {
-      activities: [] as IActivity[],
-    };
-  },
-  async mounted() {
-    this.activities = await getActivities();
   },
 });
 </script>
