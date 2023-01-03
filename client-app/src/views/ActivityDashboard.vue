15b6c3eb8176f858086b90250008aede0efe04d9
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-03 21:36:48 +0100

    dashboard component

---
+++ client-app/src/views/ActivityDashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mx-auto max-w-7xl px-4 sm:px-6">
+    <ActivityList :activities="activities" />
+  </div>
+</template>
+
+<script lang="ts">
+import ActivityList from "@/components/ActivityList.vue";
+import { getActivities } from "@/services/activitiesApi";
+import type { IActivity } from "@/types/Activity.interface";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      activities: [] as IActivity[],
+    };
+  },
+  async mounted() {
+    this.activities = await getActivities();
+  },
+  components: { ActivityList },
+});
+</script>
+
+<style scoped></style>
