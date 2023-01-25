fe76f4c48b9e04355c11c807a15e5088aac5ec93
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-25 21:10:15 +0100

    SET_ACTIVE_ACTIVITY

--- client-app/src/stores/activities.ts
+++ client-app/src/stores/activities.ts
@@ -1,15 +1,21 @@
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import { defineStore } from "pinia";
 import type { IActivity } from "@/types/Activity.interface";
 import { getActivities } from "@/services/getActivities";
 
 export const useActivitiesStore = defineStore("activities", () => {
   const activities = ref<IActivity[]>([]);
+  const activeActivity = ref<IActivity>();
 
   const FETCH_ACTIVITIES = async () => {
     const result = await getActivities();
     activities.value = result;
+    SET_ACTIVE_ACTIVITY(activities.value[0]);
   };
 
-  return { activities, FETCH_ACTIVITIES };
+  const SET_ACTIVE_ACTIVITY = (activity: IActivity) => {
+    activeActivity.value = activity;
+  };
+
+  return { activities, activeActivity, FETCH_ACTIVITIES, SET_ACTIVE_ACTIVITY };
 });
