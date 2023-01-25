e565ad55e9d3c3d023a2537f402fac6b6a179d59
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-25 21:09:55 +0100

    add button

--- client-app/src/components/ActivityItem.vue
+++ client-app/src/components/ActivityItem.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="bg-neutral-600 my-2 p-4 rounded-md max-w-[700px]">
-    <p class="text-2xl bold">{{ activity?.title }}</p>
+  <div class="bg-neutral-600 my-5 p-4 rounded-md w-full">
+    <p class="text-2xl bold">{{ activity!.title }}</p>
     <p class="text-sm text-neutral-400 mb-2">{{ activity?.date }}</p>
-    <p>{{ activity?.description }}</p>
-    <p>{{ activity?.city }}, {{ activity?.venue }}</p>
-    <div class="flex mt-2">
-      <div class="bg-indigo-600 py-1 px-2 rounded-md">
-        {{ activity?.category }}
+    <p>{{ activity!.description }}</p>
+    <p>{{ activity!.city }}, {{ activity?.venue }}</p>
+    <div class="flex mt-2 justify-between">
+      <div
+        class="bg-indigo-600 py-1 px-2 rounded-md flex justify-center items-center"
+      >
+        {{ activity!.category }}
       </div>
+      <action-button
+        type="primary"
+        text="View"
+        @click="setActiveActivity(activity!)"
+      />
     </div>
   </div>
 </template>
@@ -16,10 +23,18 @@
 import type { IActivity } from "@/types/Activity.interface";
 import type { PropType } from "vue";
 import { defineProps } from "vue";
+import { useActivitiesStore } from "@/stores/activities";
+
+import ActionButton from "@/components/ActionButton.vue";
 
 defineProps({
   activity: Object as PropType<IActivity>,
 });
+
+const activityStore = useActivitiesStore();
+const setActiveActivity = (activity: IActivity) => {
+  activityStore.SET_ACTIVE_ACTIVITY(activity);
+};
 </script>
 
 <style scoped></style>
