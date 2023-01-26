c0cda0fb31e3418569cf29bb9c6aa11459eaa72d
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-26 19:21:03 +0100

    first commit

---
+++ client-app/src/components/shared/Badge.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="border border-indigo-400 px-3 py-2 rounded-full flex justify-center items-center hover:cursor-pointer hover:bg-indigo-400 transition duration-100 ease-in-out h-fit"
+  >
+    <span class="text-sm leading-3">{{ text }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped></style>
