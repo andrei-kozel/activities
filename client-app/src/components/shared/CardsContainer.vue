ec110c6eb0a2222d8843b61e1cf4f0ea8b887bad
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-26 19:21:19 +0100

    first commit

---
+++ client-app/src/components/shared/CardsContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="computedStyles">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  styles: {
+    type: String,
+    required: false,
+    default: "",
+  },
+});
+
+const computedStyles = computed(() => {
+  return props.styles + " bg-background-300 my-5 p-4 rounded-md w-full";
+});
+</script>
