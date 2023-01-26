f3add22b4f052d4364e32cf76cf0bb0656614ec8
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-26 19:20:22 +0100

    new colors

--- client-app/tailwind.config.js
+++ client-app/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          100: "#FFFFFFDE",
+          200: "#b3b3b3",
+          300: "#282828",
+          400: "#404040",
+          500: "#181818",
+          600: "#121212",
+        },
+        primary: {
+          green: "#42b883",
+        },
+        text: {
+          dark: "#213547",
+        },
+      },
+    },
   },
   plugins: [],
 };
