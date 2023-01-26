72730c907200c5361b4806ad30014046b3251374
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-26 11:24:50 +0100

    add 2 buttons

--- client-app/src/components/ActivityDetails.vue
+++ client-app/src/components/ActivityDetails.vue
@@ -6,10 +6,14 @@
         src="https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
       />
     </div>
-    <div class="p-2">
-      <p>{{ activity?.title }}</p>
-      <p>{{ activity?.date }}</p>
+    <div class="p-2 mb-3">
+      <p class="text-xl">{{ activity?.title }}</p>
+      <p class="text-xs text-gray-400">{{ activity?.date }}</p>
       <p>{{ activity?.description }}</p>
+    </div>
+    <div class="flex">
+      <action-button type="secondary" text="Edit" class="w-full" />
+      <action-button type="secondary" text="Cancel" class="w-full" />
     </div>
   </div>
 </template>
@@ -17,6 +21,8 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { useActivitiesStore } from "@/stores/activities";
+
+import ActionButton from "./ActionButton.vue";
 
 const activityStore = useActivitiesStore();
 const activity = computed(() => activityStore.activeActivity);
