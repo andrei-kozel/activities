6615a01fcc8554360b021344dff85d9d7d1367fd
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-26 11:47:29 +0100

    first commit

---
+++ client-app/src/components/ActivityForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-full mt-5 p-2 rounded-md overflow-hidden bg-neutral-600">
+    <form>
+      <form-input placeholder="Title" :value="activity?.title" />
+      <form-input placeholder="Description" :value="activity?.description" />
+      <form-input placeholder="Date" :value="activity?.date" />
+      <form-input placeholder="City" :value="activity?.city" />
+      <form-input placeholder="Venue" :value="activity?.venue" />
+      <form-input placeholder="Category" :value="activity?.category" />
+      <div class="flex justify-end space-x-1 mt-4">
+        <action-button type="primary" text="save" />
+        <action-button type="secondary" text="cancel" />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useActivitiesStore } from "@/stores/activities";
+import { computed } from "vue";
+
+import FormInput from "@/components/shared/FormInput.vue";
+import ActionButton from "@/components/ActionButton.vue";
+
+const activityStore = useActivitiesStore();
+const activity = computed(() => activityStore.activeActivity);
+</script>
+
+<style scoped></style>
---
+++ client-app/src/components/shared/FormInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input
+      :placeholder="placeholder"
+      :value="value"
+      class="w-full p-1 rounded-sm bg-neutral-300 text-neutral-800 my-2"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  placeholder: { type: String, required: true },
+  value: { type: String, required: false, default: "" },
+});
+</script>
+
+<style scoped></style>
