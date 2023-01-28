3f4148bd88fe45e4c19360d653a407b8231a2840
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-28 13:31:43 +0100

    first commit

---
+++ client-app/src/stores/state.ts
@@ -0,0 +1,24 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import { useActivitiesStore } from "@/stores/activities";
+
+export const useState = defineStore("state", () => {
+  const editMode = ref(false);
+
+  const EDIT_MODE_ON = (id?: string) => {
+    editMode.value = true;
+    const activityStore = useActivitiesStore();
+
+    if (id == undefined) {
+      activityStore.CLEAR_ACTIVE_ACTIVITY();
+    } else {
+      activityStore.SET_ACTIVE_ACTIVITY(id);
+    }
+  };
+
+  const EDIT_MODE_OFF = () => {
+    editMode.value = false;
+  };
+
+  return { editMode, EDIT_MODE_ON, EDIT_MODE_OFF };
+});
