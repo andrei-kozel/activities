5ff23dd5dcaec05aa311dfd0e2f5d6e841a7d44d
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-28 13:31:15 +0100

    handle input change

--- client-app/src/components/shared/FormInput.vue
+++ client-app/src/components/shared/FormInput.vue
@@ -5,6 +5,7 @@
       :placeholder="placeholder"
       :value="modelValue"
       class="w-full p-1 rounded-sm bg-neutral-300 text-neutral-800 my-2"
+      @input="handleInput"
     />
   </div>
 </template>
@@ -14,4 +15,11 @@ defineProps({
   placeholder: { type: String, required: true },
   modelValue: { type: String, required: false, default: "" },
 });
+
+const emit = defineEmits(["update:modelValue"]);
+
+const handleInput = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+  emit("update:modelValue", target.value);
+};
 </script>
